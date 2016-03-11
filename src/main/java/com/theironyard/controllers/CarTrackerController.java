9eca996b024042ba197fe2c3de6b140de2ca423e
carolineevail <carolineevail@gmail.com>
2016-03-10 21:19:22 -0500

    Added / and /add-car routes, and started html

--- src/main/java/com/theironyard/controllers/CarTrackerController.java
+++ src/main/java/com/theironyard/controllers/CarTrackerController.java
@@ -1,9 +1,16 @@
 package com.theironyard.controllers;
 
+import com.theironyard.entities.Car;
+import com.theironyard.entities.User;
 import com.theironyard.services.CarRepository;
 import com.theironyard.services.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by Caroline on 3/10/16.
@@ -15,4 +22,26 @@ public class CarTrackerController {
 
     @Autowired
     UserRepository users;
+
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String home(HttpSession session, Model model) {
+        String userName = (String) session.getAttribute("userName");
+        User user = users.findFirstByName(userName);
+        if (userName != null) {
+            model.addAttribute("user", user);
+        }
+        return "home";
+    }
+
+    @RequestMapping(path = "/add-car", method = RequestMethod.POST)
+    public String addCar(HttpSession session, String carMake, String carModel, int carYear, String carColor) {
+        String userName = (String) session.getAttribute("userName");
+        User user = users.findFirstByName(userName);
+        Car car = new Car(carMake, carModel, carYear, carColor, user);
+        cars.save(car);
+        return "redirect:/";
+    }
+
+
 }
--- src/main/java/com/theironyard/services/UserRepository.java
+++ src/main/java/com/theironyard/services/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by Caroline on 3/10/16.
  */
 public interface UserRepository extends CrudRepository<User, Integer> {
+    User findFirstByName(String userName);
 }
